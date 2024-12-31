af52eae78266c9da9def4874abe3af1120f970e4
dearjh36 <dearjh36@naver.com>
2024-12-31 19:53:57 +0900

    #3 Add
    User Service 테스트 만드는중

--- spring-security-practice/src/test/java/com/project/springsecuritypractice/user/UserServiceTest.java
+++ spring-security-practice/src/test/java/com/project/springsecuritypractice/user/UserServiceTest.java
@@ -20,35 +20,41 @@ class UserServiceTest {
     private UserRepository userRepository;
 
     @Test
-    void signup() {
-        //given
-        String username = "user123";
-        String password = "password";
-        //when
-        User user = userService.signup(username, password);
-        //then
-        then(user.getId()).isNotNull(); // id가 NotNull인지 검증
-        then(user.getUsername()).isEqualTo("user123"); // 유저명이 user123인지 검증
+    void signup(){
+
+        // Given
+        String username = "user1";
+        String pw = "user1";
+
+        // When
+        User user = userService.signup(username,pw);
+
+        // Then
+        then(user.getId()).isNotNull(); // id가 NotNull 인지
+        then(user.getUsername()).isEqualTo("user1"); //유저이름이 'user1' 인지 검증
         then(user.getPassword()).startsWith("{bcrypt}"); // 패스워드가 {bcrypt}로 시작하는지 검증
-        then(user.getAuthorities()).hasSize(1); // Authorities(권한)가 1개인지 검증
+        then(user.getAuthorities()).hasSize(1); // Authorities(권한)이 1개인기 검증 (admin Or User)
         then(user.getAuthorities().stream().findFirst().get().getAuthority()).isEqualTo("ROLE_USER");
-        then(user.isAdmin()).isFalse(); // 어드민 여부가 False인지 검증
-        then(user.isAccountNonExpired()).isTrue();
+        then(user.isAdmin()).isFalse(); // 어드민이 False인지 검증
+        then(user.isAccountNonExpired()).isTrue(); // 사용자의 계정이 만료되어있는지 확인 (로그인 자격이 유효한지)
         then(user.isAccountNonLocked()).isTrue();
-        then(user.isEnabled()).isTrue();
-        then(user.isCredentialsNonExpired()).isTrue();
+        then(user.isEnabled()).isTrue(); // 활성화인지 비활성화인지
+        then(user.isCredentialsNonExpired()).isTrue(); // 인증 자격, 즉 비밀번호 만료 확인
+
     }
 
     @Test
-    void signupAdmin() {
-        //given
-        String username = "admin123";
-        String password = "password";
-        //when
-        User user = userService.signupAdmin(username, password);
-        //then
+    void signupAdmin(){
+        // Given
+        String username = "admin1";
+        String pw = "admin12";
+
+        // When
+        User user = userService.signupAdmin(username,pw);
+
+        // Then
         then(user.getId()).isNotNull();
-        then(user.getUsername()).isEqualTo("admin123");
+        then(user.getUsername()).isEqualTo("admin1");
         then(user.getPassword()).startsWith("{bcrypt}");
         then(user.getAuthorities()).hasSize(1);
         then(user.getAuthorities().stream().findFirst().get().getAuthority()).isEqualTo("ROLE_ADMIN");
@@ -57,15 +63,9 @@ class UserServiceTest {
         then(user.isAccountNonLocked()).isTrue();
         then(user.isEnabled()).isTrue();
         then(user.isCredentialsNonExpired()).isTrue();
+
     }
 
-    @Test
-    void findByUsername() {
-        //given
-        userRepository.save(new User("user123", "password", "ROLE_USER"));
-        //when
-        User user = userService.findByUsername("user123");
-        //then
-        then(user.getId()).isNotNull();
-    }
+    
+
 }
